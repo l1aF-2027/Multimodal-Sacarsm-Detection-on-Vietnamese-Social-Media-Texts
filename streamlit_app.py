9e5b80586473b1486da33b5ca3d3c76018ad98e4
l1aF-2027 <22520460@gm.uit.edu.vn>
2024-12-25 16:46:58 +0700

    change navigation bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,3 @@
-import os
-import sys
-def install_packages():
-    try:
-        import streamlit_option_menu
-    except ImportError:
-        os.system(f"{sys.executable} -m pip install streamlit-option-menu")
-
-install_packages()
 import streamlit as st
 from streamlit_option_menu import option_menu
 
