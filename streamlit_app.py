67e32feb8d688f1881dd9c1f5fd291b1788aad69
l1aF-2027 <22520460@gm.uit.edu.vn>
2024-12-25 16:55:41 +0700

    change navigation bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,51 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# Thêm tiêu đề có ảnh bằng Markdown và HTML
-st.markdown(
-    """
-    <div style="display: flex; align-items: center;">
-        <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" width="30" style="margin-right:10px;">
-        <h1 style="display: inline;">AI002</h1>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+# Custom CSS for styling
+st.markdown("""
+    <style>
+    .css-1y4p8pa {
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+    .menu-title {
+        background-image: url('https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/343567058_5683586315079218_583712912555665595_n.png?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=khKFog7hPmoQ7kNvgHLKP40&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&_nc_gid=AjVYUgFwYLBQPMso_7Cvefs&oh=00_AYARFFGGZ_XRkK93IJLRNrAkKdnBPE3qsewVZ9x3GLRwlw&oe=6771C2A6');
+        background-size: cover;
+        background-position: center;
+        padding: 20px;
+        position: relative;
+        color: white;
+        text-align: center;
+    }
+    .menu-title::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 1;
+    }
+    .menu-title h1 {
+        position: relative;
+        z-index: 2;
+        margin: 0;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
-# Menu chính
+# Custom title with logo and heading
+st.markdown("""
+    <div class="menu-title">
+        <h1>
+            <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" width="30" style="vertical-align: middle; margin-right: 10px;">
+            AI002
+        </h1>
+    </div>
+""", unsafe_allow_html=True)
+
+# Menu options
 page = option_menu(
     menu_title="",
     options=["Main Posts", "Review Posts"],
@@ -20,4 +53,3 @@ page = option_menu(
     default_index=0,
     orientation="horizontal",
 )
-
