a4582895afc802233ad30f628c4a5097b7025e67
l1aF-2027 <22520460@gm.uit.edu.vn>
2024-12-25 17:06:11 +0700

    change navigation bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,29 +8,23 @@ st.markdown("""
         padding-top: 0;
         padding-bottom: 0;
     }
-    .menu-title {
-        background-image: url('https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/343567058_5683586315079218_583712912555665595_n.png?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=khKFog7hPmoQ7kNvgHLKP40&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&_nc_gid=AjVYUgFwYLBQPMso_7Cvefs&oh=00_AYARFFGGZ_XRkK93IJLRNrAkKdnBPE3qsewVZ9x3GLRwlw&oe=6771C2A6');
-        background-size: cover;
-        background-position: center;
-        padding: 20px;
+    .banner-container {
         position: relative;
-        color: white;
-        text-align: center;
+        width: 100%;
     }
-    .menu-title::before {
-        content: '';
+    .banner-image {
+        width: 100%;
+        filter: brightness(0.7);  /* Make background darker */
+    }
+    .group-name {
         position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: rgba(0, 0, 0, 0.5);
-        z-index: 1;
-    }
-    .menu-title h1 {
-        position: relative;
+        bottom: 20px;
+        left: 20px;
+        color: white;
+        font-size: 18px;
+        font-weight: bold;
         z-index: 2;
-        margin: 0;
+        text-shadow: 2px 2px 4px rgba(0,0,0,0.5);  /* Add shadow for better readability */
     }
     /* Custom menu styling */
     .nav-link {
@@ -41,16 +35,17 @@ st.markdown("""
         background-color: #e6e6e6 !important;
     }
     .nav-link.active {
-        background-color: #AAAAAA	 !important;  /* Light blue-gray color */
+        background-color: #AAAAAA !important;
         color: #333333 !important;
     }
     </style>
 """, unsafe_allow_html=True)
 
-# Custom title with banner image
+# Custom title with banner image and group name
 st.markdown("""
-    <div style="text-align: center;">
-        <img src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/343567058_5683586315079218_583712912555665595_n.png?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=khKFog7hPmoQ7kNvgHLKP40&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&_nc_gid=AjVYUgFwYLBQPMso_7Cvefs&oh=00_AYARFFGGZ_XRkK93IJLRNrAkKdnBPE3qsewVZ9x3GLRwlw&oe=6771C2A6" style="width: 100%; margin-bottom: 0;">
+    <div class="banner-container">
+        <img class="banner-image" src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/343567058_5683586315079218_583712912555665595_n.png?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=khKFog7hPmoQ7kNvgHLKP40&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&_nc_gid=AjVYUgFwYLBQPMso_7Cvefs&oh=00_AYARFFGGZ_XRkK93IJLRNrAkKdnBPE3qsewVZ9x3GLRwlw&oe=6771C2A6">
+        <div class="group-name">Ban Học Tập Công Nghệ Phần Mềm</div>
     </div>
 """, unsafe_allow_html=True)
 
@@ -70,7 +65,7 @@ page = option_menu(
             "--hover-color": "#e6e6e6",
         },
         "nav-link-selected": {
-            "background-color": "#AAAAAA",  # Light blue-gray when selected
+            "background-color": "#AAAAAA",
         },
     }
 )
