f494982b371c0f633ceab83f12c427aa6badb002
l1aF-2027 <22520460@gm.uit.edu.vn>
2024-12-25 18:52:37 +0700

    add utility function and the content page

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,15 +114,18 @@ def show_post(post):
                 margin-bottom: 20px;
                 box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
             ">
+            
+                <!-- Image -->
+                <div style="text-align: center;">
+                    <img src="data:image/jpeg;base64,{post['image']}" style="max-width: 100%; border-radius: 10px;"/>
+                </div>
+                
                 <!-- Caption -->
                 <div style="margin-bottom: 15px;">
                     <p style="font-size: 16px; font-weight: bold; margin: 0;">{post['text']}</p>
                 </div>
                 
-                <!-- Image -->
-                <div style="text-align: center;">
-                    <img src="data:image/jpeg;base64,{post['image']}" style="max-width: 100%; border-radius: 10px;"/>
-                </div>
+                
                 
                 <!-- Timestamp -->
                 <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
