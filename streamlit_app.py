155c42f2d85cfd3707caa2714b79b4cdb2e5b42d
l1aF-2027 <22520460@gm.uit.edu.vn>
2024-12-26 18:30:32 +0700

    add some examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -517,6 +517,8 @@ if page == 'Main Posts':
                     st.success("Your post has been submitted for review!")
                 else:
                     st.error("Please upload an image and write text.")
+    else:
+        st.error("The text must be less than or equal to 256 words.")
     if (len(st.session_state.approved_posts) > 0):
         for post in st.session_state.approved_posts:
             display_post(post)       
