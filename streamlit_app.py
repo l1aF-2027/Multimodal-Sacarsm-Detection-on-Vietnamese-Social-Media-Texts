772463c2e3d7dee0fac98e2ee060d30b17471a49
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-01-24 22:22:27 +0700

    change default theme

--- streamlit_app.py
+++ streamlit_app.py
@@ -682,9 +682,9 @@ elif page == 'Review Posts':
     else:
         # Display pending posts with approve buttons
         for i, post in enumerate(st.session_state.pending_posts):
-            prediction = get_cached_prediction(os.getcwd() + post['image'], post['text'])
+            prediction = get_cached_prediction(post['image'], post['text'])
             if prediction is None:
-                prediction = classifier.predict(os.getcwd() + post['image'], post['text'])
+                prediction = classifier.predict(post['image'], post['text'])
                 save_prediction(post['image'], post['text'], prediction)
             show_post(post, index=i, prediction=prediction)
             st.markdown("---")
